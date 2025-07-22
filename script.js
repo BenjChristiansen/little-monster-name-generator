e5cca2b0c2e202a37e523ba1e15ed1d885abb516
BenjChristiansen <128442458+BenjChristiansen@users.noreply.github.com>
2025-07-22 02:46:21 -0600

    Update script.js
    Declared new prefix, noun and agentNouns variables containing arrays that will be utilized to output a "Little Monster Name".

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const firstName=document.getElementById("first-name"); 
 const lastName=document.getElementById("last-name");
 const button=document.getElementById("generate-name");
+const prefix=["The", "Queen", "Countess", "Lord", "Princess", "Doctor", "Supreme", "Baroness", "Madame", "Sassy", "Royal", "Sis", "Count", "Monsieur", "A One", "Madmoisette", "Mister", "Mother", "Father", "Professor", "Lady", "The Incomparable", "Infamous", "Seniorita", "Governess", "Judge"];
+const nouns = ["Zombie", "Joanne", "Venus", "Sour Candy", "Disco", "Harlequin", "Patrizia", "Gucci", "Mayhem", "Shadow", "Dance", "Beast", "Dope", "Swine", "Aura", "Fame", "Pinot Grigio", "Gypsy", "Mary Jane", "Beat", "Celebrity", "Unicorn", "Fashion", "Judas", "Diamond", "Enigma"];
+const agentNouns= ["Guzzler", "Dingler", "Dribbler", "Slayer", "Killah", "Haunter", "Screecher", "Biter", "Caller", "Romancer", "Lover", "Kisser", "Sinner", "Chaser", "Clapper", "Popper", "Raver", "Drinker", "Hooker", "Bedazzler", "Bumper", "Humper", "Hacker", "Fluffer", "Sniffer", "Smoosher"];
 
 function generateName(first, last){
   const cleanedFirst=first.trim().toUpperCase();
